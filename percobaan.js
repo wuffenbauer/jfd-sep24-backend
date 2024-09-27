17e94789bdf5230862477b198af5e5ebea5aeee3
wuffenbauer <wuffenbauer@gmail.com>
2024-09-27 21:42:29 +0700

    edit console.log & nambah komentar

--- percobaan.js
+++ percobaan.js
@@ -6,5 +6,8 @@ http.createServer(function(request, response) {
     response.writeHead(200, {'Content-type': 'text/plain'})
     response.end('Hello, this script is generated from NodeJS backend.')
 }).listen(3000, function() {
-    console.log('Server is on, go to http://localhost:3000')
+    console.log('Server is active, go to http://localhost:3000')
 })
+
+// tes nambah komentar
+// kirim ke 2x
