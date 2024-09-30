86fcd1fa26d3c5532b01d06a8cf031cbbff062c3
wuffenbauer <wuffenbauer@gmail.com>
2024-09-30 19:46:23 +0700

    mendeteksi halaman atau url web

--- percobaan.js
+++ percobaan.js
@@ -3,11 +3,32 @@
 const http = require('http')
 
 http.createServer(function(request, response) {
-    response.writeHead(200, {'Content-type': 'text/plain'})
-    response.end('Hello, this script is generated from NodeJS backend.')
+    response.writeHead(200, {'Content-type': 'text/html'})
+    // halaman utama
+    if (request.url == '/') {        
+        response.end('<h1>Welcome to My Website.</h1>')
+    } else if (request.url == '/profil') {
+        response.end(
+            `<h1>Profil Saya</h1>
+            <ul>
+                <li>Nama lengkap: Amirah Puspadewi</li>
+                <li>Nama panggilan: Ami</li>
+                <li>Alamat: Pamulang, Tangerang Selatan</li>
+                <li>Pekerjaan: Mahasiswa, Freelancer</li>
+            </ul>`
+        )
+    } else if (request.url == '/hubungi-saya') {
+        response.end(
+            `<h1>Hubungi Saya</h1>
+            <ul>
+                <li>No. WA: 0856-3559-890</li>
+                <li>Email: wuffenbauer@gmail.com</li>
+            </ul>`
+        )
+    // untuk menangani url yang tidak ada:
+    } else {
+        response.end('<h1>404: Halaman tidak ditemukan.</h1>')
+    }
 }).listen(3000, function() {
     console.log('Server is on, go to http://localhost:3000')
 })
-
-// tes nambah komentar
-// kirim ke 2x
