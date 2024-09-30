24be43354243df6b8ec7d27225f73718664b342d
wuffenbauer <wuffenbauer@gmail.com>
2024-09-30 20:45:39 +0700

    file html terpisah

--- percobaan.js
+++ percobaan.js
@@ -1,40 +1,23 @@
 // memanggil modul bawaan dari node.js, yaitu http
 // untuk membuat server http
-const http = require('http')
+const http  = require('http')
+const fs    = require('fs')
 
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-type': 'text/html'})
     // halaman utama
     if (request.url == '/') {        
-        response.end(`
-            <h1>Selamat datang di situs saya</h1>
-            <br>
-            <a href="/profil">Lihat profil`
-        )
-    } else if (request.url == '/profil') {
-        let tahunLahir  = 1992
-        let tahunIni    = 2024
-        let umur        = tahunIni - tahunLahir
-        response.end(
-            `<h1>Profil Saya</h1>
-            <ul>
-                <li>Nama lengkap: Amirah Puspadewi</li>
-                <li>Nama panggilan: Ami</li>
-                <li>Alamat: Pamulang, Tangerang Selatan</li>
-                <li>Pekerjaan: Mahasiswa, Freelancer</li>
-                <li>Tempat, tanggal lahir: Jakarta, 13 Juni ${tahunLahir}</li>
-                <li>Umur: ${umur} tahun</li>
-            </ul>
-            <br>
-            <a href="/">Balik ke beranda`
-        )
-    // halaman hubungi saya
-    } else if (request.url == '/hubungi-saya') {
+        fs.createReadStream('./view/halaman-utama.html').pipe(response)
+    } // halaman profil
+    else if (request.url == '/profil') {
+        fs.createReadStream('./view/halaman-profil.html').pipe(response)
+    } // halaman hubungi saya
+    else if (request.url == '/hubungi-saya') {
         let kontak = {
-            wa: '+628563559890',
-            email: 'wuffenbauer@gmail.com',
+            wa      : '+628563559890',
+            email   : 'wuffenbauer@gmail.com',
             linkedin: 'linkedin/apuspadw',
-            ig: '@wuffenbauer',
+            ig      : '@wuffenbauer',
         }
         response.end(
             `<h1>Hubungi Saya</h1>
@@ -47,8 +30,9 @@ http.createServer(function(request, response) {
             <br>
             <a href="/">Balik ke beranda`
         )
-    // untuk menangani url yang tidak ada:
-    } else {
+    
+    } // untuk menangani url yang tidak ada:
+    else {
         response.end('<h1>404: Halaman tidak ditemukan.</h1>')
     }
 }).listen(3000, function() {
