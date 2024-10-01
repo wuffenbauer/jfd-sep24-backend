40b238e824e94c0893fe09a756a752d4e2107a17
wuffenbauer <wuffenbauer@gmail.com>
2024-10-01 21:52:58 +0700

    view engine ejs & konsumsi data dari backend

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express   = require('express')
 const app       = express()
 
+app.set('view engine', 'ejs')   // setting penggunaan template engine untuk express
+app.set('views', './view-ejs')  // setting penggunaan folder untuk menyimpan file .ejs
+
+// function render('nama file')
+// nama file-nya wajib berekstensi .ejs
+// otomatis mengambil file .ejs yang ada di folder view-ejs
+
 // bikin rute
 app.get('/', function(req, res) {
-    res.send('<h1>Hello World</h1>')
+    res.render('beranda')
 })
 
 app.get('/pendidikan', function(req, res) {
-    res.send('<h1>Riwayat Pendidikan</h1>')
+    let profil = {
+        nama: 'Amirah Puspadewi',
+        s1  : 'Universitas Terbuka - Sastra Inggris Bidang Minat Penerjemahan',
+        sma : 'SMAN 2 Kota Tangerang Selatan - IPS',
+    }
+    res.render('page-pendidikan', profil)
 })
 
 app.get('/karyawan', function(req, res) {
-    res.send('<h1>List Karyawan</h1>')
+    res.render('page-karyawan')
 })
 
-// 
+// sambungkan ke server
 app.listen(3000, () => {
     console.log('Server aktif, buka http://localhost:3000')
 })
